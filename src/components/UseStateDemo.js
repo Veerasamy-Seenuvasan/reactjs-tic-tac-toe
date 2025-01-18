8e185382e81e13b98f8bb451b488eba7866a2c7f
Veerasamy Seenuvasan <iamveera8@gmail.com>
2025-01-18 04:44:30 +0000

    18 Jan 2025, 10:14

---
+++ src/components/UseStateDemo.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+
+export default function UseStateDemo() {
+    const {counter,setCounter} = useState(5);
+  return (
+    <div>
+      <span>counter</span>
+      <button onClick={()=>{
+        setCounter(counter+5);
+        setCounter(counter+5);
+        alert(counter);
+        setCounter(counter+5);
+        setCounter(counter+5);
+
+      }}>Increment</button>
+    </div>
+  )
+}
