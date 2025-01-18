4b31bbb0e9c498123341581b6b764b65c7971745
Veerasamy Seenuvasan <iamveera8@gmail.com>
2025-01-18 05:50:44 +0000

    18 Jan 2025, 11:20

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import UseStateDemo from '../src/components/UseStateDemo';
+import StateDemoCC from '../src/components/StateDemoCC';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <UseStateDemo />
+    <StateDemoCC />
   </React.StrictMode>
 );
