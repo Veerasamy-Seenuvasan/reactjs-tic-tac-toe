beb157a65cf7c6d2d379a6093403c4d16a469ed5
Veerasamy Seenuvasan <iamveera8@gmail.com>
2025-01-18 05:50:03 +0000

    18 Jan 2025, 11:20

--- src/components/StateDemoCC.js
+++ src/components/StateDemoCC.js
@@ -1,11 +1,46 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
-export default class StateDemoCC extends Component {
+class UseStateDemoClass extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 5
+    };
+  }
+
+  increment1 = () => {
+    this.setState(
+      { counter: this.state.counter + 5 },
+      () => {
+        this.setState({ counter: this.state.counter + 5 });
+        alert(this.state.counter);
+        this.setState({ counter: this.state.counter + 5 });
+        this.setState({ counter: this.state.counter + 5 });
+      }
+    );
+  };
+
+  increment2 = () => {
+    this.setState(
+      (prevState) => ({ counter: prevState.counter + 5 }),
+      () => {
+        this.setState((prevState) => ({ counter: prevState.counter + 5 }));
+        alert(this.state.counter);
+        this.setState((prevState) => ({ counter: prevState.counter + 5 }));
+        this.setState((prevState) => ({ counter: prevState.counter + 5 }));
+      }
+    );
+  };
+
   render() {
     return (
       <div>
-        
+        <span>{this.state.counter}</span>
+        <button onClick={this.increment1}>Increment 1</button>
+        <button onClick={this.increment2}>Increment 2</button>
       </div>
-    )
+    );
   }
 }
+
+export default UseStateDemoClass;
