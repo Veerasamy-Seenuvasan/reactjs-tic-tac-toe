b3af9502833d9ea076cac3e56a936d44189e9c38
Veerasamy Seenuvasan <iamveera8@gmail.com>
2025-01-18 05:05:24 +0000

    18 Jan 2025, 10:35

--- src/components/UseStateDemo.js
+++ src/components/UseStateDemo.js
@@ -2,6 +2,13 @@ import React, { useState } from 'react'
 
 export default function UseStateDemo() {
     const [counter,setCounter] = useState(5);
+
+    useEffect(() => {
+      // This will run whenever 'counter' is updated
+      alert(`Counter updated to: ${counter}`);
+    }, [counter]);
+
+    
   return (
     <div>
       <span>{counter}</span>
@@ -15,7 +22,7 @@ export default function UseStateDemo() {
       }}>Increment</button> {/* Alert with 5, final counter value 5 */}
       <button onClick={()=>{
         setCounter(counter => counter+5);
-        setCounter(counter => counter+5,()=>{ alert(counter)});
+        setCounter(counter => counter+5);
         setCounter(counter => counter+5);
         setCounter(counter => counter+5);
 
